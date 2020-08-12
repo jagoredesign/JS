22691e0cf7f9b718e3ebf6a0a519a239e41c8b88
jagoredesign <68729184+jagoredesign@users.noreply.github.com>
2020-08-12 16:06:20 +0700

    Update and rename style.js to warning.js

---
+++ Jago Safelink/warning.js
@@ -0,0 +1,32 @@
+<!--
+
+var message="Hmm! Keciduk ni.. Maaf Situs ini dilarang untuk Copy-Paste, CTRL+U, dan Klik Kanan";
+
+///////////////////////////////////
+function clickIE4(){
+if (event.button==2){
+alert(message);
+return false;
+}
+}
+
+function clickNS4(e){
+if (document.layers||document.getElementById&&!document.all){
+if (e.which==2||e.which==3){
+alert(message);
+return false;
+}
+}
+}
+
+if (document.layers){
+document.captureEvents(Event.MOUSEDOWN);
+document.onmousedown=clickNS4;
+}
+else if (document.all&&!document.getElementById){
+document.onmousedown=clickIE4;
+}
+
+document.oncontextmenu=new Function("alert(message);return false")
+
+// -->
